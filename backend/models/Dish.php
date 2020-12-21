491399dc43121a03e8a60dbaa62d214ac9ccdae9
DESKTOP-0SUMSN0\Kareem <karafat1998@gmail.com>
2020-12-21 22:30:19 +0200

    [BE] Add dish Layer

---
+++ backend/models/Dish.php
@@ -0,0 +1,59 @@
+<?php
+
+class Dish
+{
+
+    private $id;
+    private $name;
+    private $price;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * @param mixed $price
+     */
+    public function setPrice($price): void
+    {
+        $this->price = $price;
+    }
+
+
+}
